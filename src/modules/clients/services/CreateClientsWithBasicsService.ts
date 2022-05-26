5f5979f8176a00b4312237f79bb5d087fa01e03d
GUILHERME LUIZ MAIA PINTO <68974994+guiluizmaia@users.noreply.github.com>
2022-05-26 09:57:40 -0300

    Update CreateClientsWithBasicsService.ts

--- src/modules/clients/services/CreateClientsWithBasicsService.ts
+++ src/modules/clients/services/CreateClientsWithBasicsService.ts
@@ -4,7 +4,7 @@ import IClientsRepository from '@modules/clients/repositories/IClientsRepository
 import Clients from '@modules/clients/infra/typeorm/schemas/Clients';
 import IHistoricClientsRepository from '../repositories/IHistoricClientsRepository';
 import ICryptHash from '@infra/utils/CryptHash/ICryptHash';
-import { descriptografar } from '@infra/utils/cryptografar';
+import { criptografar } from '@infra/utils/cryptografar';
 
 @injectable()
 class CreateClientsWithBasicsService {
@@ -20,6 +20,45 @@ class CreateClientsWithBasicsService {
   public async execute(data: IClientsBasicDtos): Promise<Clients> {
     data.password = await this.cryptHash.create(data.password);
 
+    if (data.name) {
+      data.name = criptografar(data.name);
+    }
+
+    if (data.sex) {
+      data.sex = criptografar(data.sex);
+    }
+
+    if (data.genrer) {
+      data.genrer = criptografar(data.genrer);
+    }
+
+    if (data.document) {
+      data.document = criptografar(data.document);
+    }
+
+    if (data.rg) {
+      data.rg = criptografar(data.rg);
+    }
+
+    if (data.address && data.address.cep) {
+      data.address.cep = criptografar(data.address.cep);
+    }
+
+    if (data.address && data.address.street) {
+      data.address.street = criptografar(data.address.street);
+    }
+
+    if (data.address && data.address.district) {
+      data.address.district = criptografar(data.address.district);
+    }
+
+    if (data.address && data.address.city) {
+      data.address.city = criptografar(data.address.city);
+    }
+
+    if (data.address && data.address.state) {
+      data.address.state = criptografar(data.address.state);
+    }
     const created = await this.clientsRepository.create(data);
     this.historicClientsRepository.create({
       type: 'CREATED',
