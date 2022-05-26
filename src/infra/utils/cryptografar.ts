15779ffe248e267cf45f524f2de3da846904afde
GUILHERME LUIZ MAIA PINTO <68974994+guiluizmaia@users.noreply.github.com>
2022-05-26 10:26:42 -0300

    Update cryptografar.ts

--- src/infra/utils/cryptografar.ts
+++ src/infra/utils/cryptografar.ts
@@ -1,15 +1,17 @@
 import crypto from 'crypto';
 const algorithm = 'aes-256-ctr';
-const pwd = "chave"
- 
-export function criptografar(text) {
- const cipher = crypto.createCipher('aes-256-cbc', "chave");
- const crypted = cipher.update(String(text), 'utf8', 'hex')
- return crypted
+const pwd = 'chave';
+
+export function criptografar(text: any) {
+  const cipher = crypto.createCipher('aes-256-ctr', 'chave');
+  const crypted = cipher.update(text, 'utf8', 'hex');
+  console.log(crypted);
+
+  return crypted;
 }
- 
-export function descriptografar(text) {
- const decipher = crypto.createDecipher('aes-256-cbc', "chave");
- const plain = decipher.update(text, 'hex', 'utf8')
- return plain
+
+export function descriptografar(text: any) {
+  const decipher = crypto.createDecipher('aes-256-ctr', 'chave');
+  const plain = decipher.update(text, 'hex', 'utf8');
+  return plain;
 }
